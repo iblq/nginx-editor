0b4aa9aaf116db4d78880743ca83ff563ff80277
baolq <luoqiang.bao@gmail.com>
2020-01-21 17:09:56 +0800

    feat: 样式调整

--- app/core/tools/consts.ts
+++ app/core/tools/consts.ts
@@ -41,7 +41,7 @@ export const DEFAULT_WINDOW_OPTIONS: BrowserWindowConstructorOptions = {
 
 export const DEFAULT_INITIAL_CONFIG: CreateConfig = {
   showSidebar: false,
-  showTitlebar: true,
+  showTitlebar: false,
   autoShow: true,
   delayToShow: 10,
 }
--- app/src/components/app-sidebar/app-sidebar.tsx
+++ app/src/components/app-sidebar/app-sidebar.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Icon, Tooltip } from 'antd'
+import { withStore } from '@/src/components'
 
 import AppSideMenus from './side-menus.json'
 import './app-sidebar.less'
@@ -7,6 +8,15 @@ import './app-sidebar.less'
 type SideMenuItem = typeof AppSideMenus[0]
 
 export class AppSidebar extends React.Component {
+  state = {
+    currentPath: '#/',
+  }
+
+  onMenuSelect = (path: string) => {
+    console.log(path)
+    this.setState({ currentPath: path })
+  }
+
   render() {
     return (
       <div className="app-sidebar">
@@ -20,12 +30,19 @@ export class AppSidebar extends React.Component {
   }
 
   renderMenuItem = ({ icon, path, title }: SideMenuItem) => {
+    const { currentPath } = this.state
     return (
-      <Tooltip key={path} overlayClassName="side-menu-item-tooltip" placement="right" title={title}>
-        <a className="side-menu-item" href={path}>
-          <Icon type={icon} className="fs-24" />
-        </a>
-      </Tooltip>
+      // <Tooltip key={path} overlayClassName="side-menu-item-tooltip" placement="right" title={title}>
+      <a
+        key={path}
+        className={`side-menu-item ${path === currentPath ? 'active' : ''}`}
+        href={path}
+        onClick={() => this.onMenuSelect(path)}
+      >
+        <Icon className="side-menu-icon" type={icon} />
+        <span className="side-menu-title">{title}</span>
+      </a>
+      // </Tooltip>
     )
   }
 } // class AppSidebar end
